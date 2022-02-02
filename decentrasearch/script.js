b72d09ac21db8c8f4d782e4c37691a6d84d6f2a0
Swivro Robot <54741819+swivro-robot@users.noreply.github.com>
2022-02-02 22:27:31 +0000

    Update script.js

--- decentrasearch/script.js
+++ decentrasearch/script.js
@@ -28,7 +28,7 @@ links[25] = "https://searx.webheberg.info/?preferences=eJx1VlGP2zYM_jXnFyOHtd2w9
      
 function openLink() {
   var i = ~~(Math.random() * links.length);
-  window.location.href = links[i] + "&q=" + document.getElementsByClassName("searchsearx")[0].value;
+  window.openlinks[i] + "&q=" + document.getElementsByClassName("searchsearx")[0].value;
   return false;
 }
 
