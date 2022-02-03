5c631227d3c5083821542ae4126f47bac19a254e
Swivro Robot <54741819+swivro-robot@users.noreply.github.com>
2022-02-03 02:58:52 +0000

    Update script.js

--- decentrasearch/script.js
+++ decentrasearch/script.js
@@ -1,3 +1,21 @@
+document.cookie = "categories=general;";
+document.cookie = "language=en;";
+document.cookie = "locale=en;";
+document.cookie = "autocomplete=duckduckgo;";
+document.cookie = "image_proxy=1;";
+document.cookie = "method=POST;";
+document.cookie = "safesearch=0;";
+document.cookie = "theme=oscar;";
+document.cookie = "results_on_new_tab=0;";
+document.cookie = "doi_resolver=oadoi.org;";
+document.cookie = "oscar-style=logicodev;";
+document.cookie = "advanced_search=0;";
+document.cookie = "disabled_engines= ;";
+document.cookie = "enabled_engines= ddg definitions__general,yahoo__general,google__general,bing__general,qwant__general,gigablast__general,reddit__general,startpage__general,mojeek__general,google play apps__files,google play movies__videos,duckduckgo__general;";
+document.cookie = "disabled_plugins= ;";
+document.cookie = "enabled_plugins= ;";
+document.cookie = "tokens= ;";
+
 let links = [];
 links[0] = "https://search.ononoki.org/";
 links[1] = "https://searx.be/";
@@ -9,23 +27,6 @@ links[6] = "https://swag.pw/";
 links[7] = "https://searx.prvcy.eu/";
 
 function openLink() {
-  document.cookie = "categories=general;";
-  document.cookie = "language=en;";
-  document.cookie = "locale=en;";
-  document.cookie = "autocomplete=duckduckgo;";
-  document.cookie = "image_proxy=1;";
-  document.cookie = "method=POST;";
-  document.cookie = "safesearch=0;";
-  document.cookie = "theme=oscar;";
-  document.cookie = "results_on_new_tab=0;";
-  document.cookie = "doi_resolver=oadoi.org;";
-  document.cookie = "oscar-style=logicodev;";
-  document.cookie = "advanced_search=0;";
-  document.cookie = "disabled_engines= ;";
-  document.cookie = "enabled_engines= ddg definitions__general,yahoo__general,google__general,bing__general,qwant__general,gigablast__general,reddit__general,startpage__general,mojeek__general,google play apps__files,google play movies__videos,duckduckgo__general;";
-  document.cookie = "disabled_plugins= ;";
-  document.cookie = "enabled_plugins= ;";
-  document.cookie = "tokens= ;";
   var i = ~~(Math.random() * links.length);
   window.location.href = (links[i] + "?q=" + document.getElementsByClassName("searchsearx")[0].value);
   return false;
